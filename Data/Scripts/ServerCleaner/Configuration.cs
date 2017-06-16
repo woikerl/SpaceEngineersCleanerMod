b4af06c25c9eda313d2d0dc066cb67576f4cb72d
StellarwhimsRepo <tostitogaming@live.com>
2017-06-15 20:11:50 -0500

    cleanup
    cleanup

--- Data/Scripts/ServerCleaner/Configuration.cs
+++ Data/Scripts/ServerCleaner/Configuration.cs
@@ -6,7 +6,7 @@ namespace ServerCleaner
 	{
 
         public bool BlockToggle_Enabled = false;
-        public int BlockToggle_Interval = 30 * 60 * 1000;
+        public int BlockToggle_Interval = 33 * 60 * 1000;
         [XmlArrayItem(ElementName = "BlockType", Type = typeof(string))]
         public string[] BlockToggle_BlockNames = { "MyObjectBuilder_MyProgrammableBlock", "MyObjectBuilder_Projector", "MyObjectBuilder_TimerBlock", "MyObjectBuilder_SensorBlock","MyObjectBuilder_VirtualMass","MyObjectBuilder_SpaceBall","MyObjectBuilder_OreDetector","MyObjectBuilder_GravityGenerator","MyObjectBuilder_GravityGeneratorSphere","MyObjectBuilder_Decoy","MyObjectBuilder_MotorStator","MyObjectBuilder_MotorAdvancedStator","MyObjectBuilder_ExtendedPistonBase","MyObjectBuilder_PistonBase","MyObjectBuilder_Drill","MyObjectBuilder_ShipWelder","MyObjectBuilder_ShipGrinder" };
         /*
@@ -31,14 +31,14 @@ namespace ServerCleaner
         public bool BlockToggle_MessageAdminsOnly = true;
 
         public bool DeletePirates_Enabled = false;
-        public int DeletePirates_Interval = 60 * 60 * 1000;
+        public int DeletePirates_Interval = 58 * 60 * 1000;
         public long DeletePirates_NPC_IdentityId = 0;
         public double DeletePirates_PlayerDistanceThresholdForWarning = 1000;
         public double DeletePirates_PlayerDistanceThresholdForDeletion = 8000;
         public bool DeletePirates_MessageAdminsOnly = true;
 
         public bool StopAllShips_Enabled = true;
-        public int StopAllShips_Interval = 40 * 60 * 1000;
+        public int StopAllShips_Interval = 42 * 60 * 1000;
         
 		public bool FloatingObjectDeletion_Enabled = true;
 		public int FloatingObjectDeletion_Interval = 7 * 60 * 1000;
@@ -46,19 +46,19 @@ namespace ServerCleaner
 		public bool FloatingObjectDeletion_MessageAdminsOnly = true;
 
 		public bool UnownedGridDeletion_Enabled = true;
-		public int UnownedGridDeletion_Interval = 9 * 60 * 1000;
+		public int UnownedGridDeletion_Interval = 21 * 60 * 1000;
 		public double UnownedGridDeletion_PlayerDistanceThreshold = 500;
 		public int UnownedGridDeletion_BlockCountThreshold = 50;
 		public bool UnownedGridDeletion_MessageAdminsOnly = true;
 
 		public bool DamagedGridDeletion_Enabled = true;
-		public int DamagedGridDeletion_Interval = 10 * 60 * 1000;
+		public int DamagedGridDeletion_Interval = 11 * 60 * 1000;
 		public double DamagedGridDeletion_PlayerDistanceThreshold = 500;
 		public int DamagedGridDeletion_BlockCountThreshold = 5;
 		public bool DamagedGridDeletion_MessageAdminsOnly = true;
 
 		public bool RespawnShipDeletion_Enabled = true;
-		public int RespawnShipDeletion_Interval = 11 * 60 * 1000;
+		public int RespawnShipDeletion_Interval = 12 * 60 * 1000;
 		[XmlArrayItem(ElementName = "GridName", Type = typeof(string))]
 		public string[] RespawnShipDeletion_GridNames = { "Atmospheric Lander mk.1", "RespawnShip", "RespawnShip2" };
 		public double RespawnShipDeletion_PlayerDistanceThresholdForWarning = 50;
